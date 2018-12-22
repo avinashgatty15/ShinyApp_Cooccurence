6042427229bc1f54395efc44bf13d12b437f9a3d
avinashgatty15 <42409362+avinashgatty15@users.noreply.github.com>
2018-12-22 15:57:51 +0530

    Update ShinyApp_Co-occurence_Prereq.R

--- ShinyApp_Co-occurence_Prereq.R
+++ ShinyApp_Co-occurence_Prereq.R
@@ -3,9 +3,12 @@ try(require("nFactors")||install.packages("udpipe"))
 try(require("shiny")||install.packages("textcat"))
 try(require("shiny")||install.packages("dplyr"))
 try(require("shiny")||install.packages("stringr"))
+try(require("shiny")||install.packages("ggraph"))
 
 library("shiny")
 library("udpipe")
 library("textcat")
 library("dplyr")
 library("stringr")
+library("ggraph")
+
